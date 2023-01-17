9dfe462c526add9c77eef36cf8b2cb11a15c19fc
David Connelly <david.webguy@gmail.com>
2023-01-17 21:27:58 +0000

    correction

---
+++ video1/notes/my_notes.php
@@ -0,0 +1,14 @@
+
+C:\Users\User\Desktop\react_stuff
+λ npx babel --watch src --out-dir . --presets react-app/prod
+You have mistakenly installed the `babel` package, which is a no-op in Babel 6.
+Babel's CLI commands have been moved from the `babel` package to the `babel-cli` package.
+
+    npm uninstall babel
+    npm install --save-dev babel-cli
+
+See http://babeljs.io/docs/usage/cli/ for setup instructions.
+
+C:\Users\User\Desktop\react_stuff
+λ
+
